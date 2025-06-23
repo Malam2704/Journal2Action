0c39eb26f1920600f3ed3764c19b8be8d711ad10
Mohammed Alam <ma3655@g.rit.edu>
2025-06-22 23:08:22 -0700

    day 2 done early

--- freestyleproblems/day1.py
+++ freestyleproblems/day1.py
@@ -16,3 +16,13 @@ def filter_odd_numbers(listy):
 
 print(filter_even_numbers([1,2,3,4,5,6,7,8,9,10]))
 print(filter_odd_numbers([1,2,3,4,5,6,7,8,9,10]))
+
+# Duplicate Filters
+def remove_duplicates(nums):
+    my_set = set()
+    for i in nums:
+        if i not in my_set:
+            my_set.add(i)
+    return list(my_set)
+
+print(remove_duplicates([5,5,5,5,6,6,7,8,9]))
